fe7a4316424013a501e72dda8065c8b3a6fef126
Yao Yuan <yuan.yao@telecom-bretagne.eu>
2016-12-18 20:38:45 +0100

    bw managerBetting finish

--- org.eclipse.uml.to.java/src/main/java/Betting/managerBetting.java
+++ org.eclipse.uml.to.java/src/main/java/Betting/managerBetting.java
@@ -4,10 +4,17 @@ import exceptions.BadParametersException;
 
 public class managerBetting {
 	@SuppressWarnings("unused")
+	// length of username needs to be 6 
+	
 	private int LONG_USERNAME = 6;
+	//rule for username
+	
 	private String REGEX_USERNAME = "^[0-9A-Za-z]{6}$";
 	@SuppressWarnings("unused")
+	//length of password needs to be 8
+	
 	private int LONG_PASSWORD = 8;
+	// rule for password
 	private String REGEX_PASSWORD = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8}$";
 	private String username;
 	private String password;
@@ -17,20 +24,29 @@ public class managerBetting {
 		this.username = username;
 		System.out.println("Username is finish");
 		}
+		
+		//length of username wrong
 		if(username.length()!=6){
 			throw new BadParametersException("LONG_USERNAME Wrong");
 		}
+		
+		//rule of username wrong
 		if(!username.matches(REGEX_USERNAME)){
 			throw new BadParametersException("REGEX_USERNAME Wrong");
 		}
 
+	
 		if(pwdMngr.length()==8&&pwdMngr.matches(REGEX_PASSWORD)){
 		this.password = pwdMngr;
 		System.out.println("Password is finish");
 		}
+		
+		//length of password wrong
 		if(pwdMngr.length()!=8){
 			throw new BadParametersException("LONG_PASSWORD Wrong");
 		}
+		
+		//rule of password wrong
 		if(!pwdMngr.matches(REGEX_PASSWORD)){
 			throw new BadParametersException("REGEX_PASSWORD Wrong");
 		}
